0b7441d979b94e4bdd413e8657ba359d950e6563
Miguel Amaya <miguel.amaya99@gmail.com>
2025-06-27 20:54:19 -0500

    Add nginx configuration

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,25 +1,28 @@
 #!/bin/sh
 
+# Iniciar Nginx sin SSL primero
+nginx
+
 # Esperar a que el servicio de la app esté disponible
 until curl -s app:8000 > /dev/null; do
     echo "Esperando a que la aplicación esté disponible..."
     sleep 5
 done
 
-# Verificar si ya existen certificados
-if [ ! -d "/etc/letsencrypt/live" ]; then
-    # Obtener certificados SSL
-    certbot --nginx \
-            --non-interactive \
-            --agree-tos \
-            --email miguel.amaya99@gmail.com \
-            --domains ec2-18-231-178-130.sa-east-1.compute.amazonaws.com \
-            --redirect
-fi
+# Obtener certificado SSL
+certbot --nginx \
+        --non-interactive \
+        --agree-tos \
+        --email miguel.amaya99@gmail.com \
+        --domains ec2-18-231-178-130.sa-east-1.compute.amazonaws.com \
+        --redirect
 
-# Iniciar el cron para la renovación automática
+# Configurar la renovación automática
 echo "0 0 1 * * certbot renew --quiet" > /etc/crontabs/root
 crond
 
-# Iniciar Nginx en primer plano
+# Recargar Nginx para aplicar la configuración SSL
+nginx -s reload
+
+# Mantener el contenedor ejecutándose
 nginx -g "daemon off;"
